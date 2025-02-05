a0d92b47e882ab9e18ea83601106e4bf289e2c53
Vryllon <noahvader@yahoo.com>
2025-02-05 10:06:26 -0500

    Added automatic spawing of ability orbs

---
+++ Assets/Scripts/ManageOrbs.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManageOrbs : MonoBehaviour
+{
+
+    public GameObject orb;
+    public float secBetweenSpawn = 5f;
+    public bool endGame = false;
+    public Vector2 spawnRangeX;
+    public float spawnY = 4;
+
+    private float tempTime = 0;
+
+    private void Update()
+    {
+        tempTime += Time.deltaTime;
+
+        if (tempTime >= secBetweenSpawn)
+        {
+            spawnOrb();
+            tempTime = 0;
+        }
+    }
+    private void spawnOrb()
+    {
+        if (!endGame)
+        {
+            GameObject temp = Instantiate(orb, new Vector3(Random.Range(spawnRangeX[0], spawnRangeX[1]), spawnY, 0), transform.rotation);
+        }
+    }
+}
