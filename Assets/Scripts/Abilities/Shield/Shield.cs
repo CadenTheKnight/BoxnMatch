0f28f6922169d0aeff43801ed628cba7ece29a04
Vryllon <noahvader@yahoo.com>
2025-03-31 12:40:56 -0400

    Added SFX for each currently implimented ability

--- Assets/Scripts/Abilities/Shield/Shield.cs
+++ Assets/Scripts/Abilities/Shield/Shield.cs
@@ -6,13 +6,32 @@ using UnityEngine;
 public class Shield : MonoBehaviour
 {
     public AbilityDirection dir;
+    private AudioSource audiosource;
+    [SerializeField] private float destroyTime = 2;
+
+    private void Start()
+    {
+        audiosource = GetComponent<AudioSource>();
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.GetComponent<DamageObject>())
         {
-
-            Destroy(gameObject);
+            delete();
         }
     }
+
+    public IEnumerator handleDelete()
+    {
+        audiosource.Play();
+        yield return new WaitForSeconds(destroyTime);
+        Destroy(gameObject);
+    }
+
+    public void delete()
+    {
+        StartCoroutine(handleDelete());
+    }
+
 }
