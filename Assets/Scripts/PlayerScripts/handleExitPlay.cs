c9f7be4199a03402b77583db3558347f8f7d7d59
Vryllon <noahvader@yahoo.com>
2025-04-21 20:51:18 -0400

    Added respawns to be intial spawn points

--- Assets/Scripts/PlayerScripts/handleExitPlay.cs
+++ Assets/Scripts/PlayerScripts/handleExitPlay.cs
@@ -13,6 +13,24 @@ public class handleExitPlay : MonoBehaviour
     [SerializeField] float debounceTimer;
 
     private ArrayList debounceList = new();
+    private ArrayList respawnPoints = new();
+
+    private void Start()
+    {
+        StartCoroutine(GatherRespawnPoints());
+    }
+
+    struct RespawnPosition
+    {
+        public GameObject gm;
+        public Vector2 pos;
+
+        public RespawnPosition(GameObject gm, Vector3 position) : this()
+        {
+            this.gm = gm;
+            this.pos = position;
+        }
+    }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
@@ -25,33 +43,41 @@ public class handleExitPlay : MonoBehaviour
 
         if (colObj.CompareTag("Player")) // handle player exiting
         {
-            Debug.Log("Handle Exit 1 " + colObj.name);
+            //Debug.Log("Handle Exit 1 " + colObj.name);
 
             // Add players to the debounce list
             StartCoroutine(HandleDebouncing(colObj));
-            Debug.Log("Handle Exit 2 " + colObj.name);
+            //Debug.Log("Handle Exit 2 " + colObj.name);
 
             GetComponent<AudioSource>().Play();
-            Debug.Log("Handle Exit 3 " + colObj.name);
+            //Debug.Log("Handle Exit 3 " + colObj.name);
 
             //Debug.Log("exit");
             DamageableObject damagableObj = colObj.GetComponent<DamageableObject>();
-            Debug.Log("Handle Exit 4 " + colObj.name);
+            //Debug.Log("Handle Exit 4 " + colObj.name);
 
             colObj.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
-            Debug.Log("Handle Exit 5 " + colObj.name);
+            //Debug.Log("Handle Exit 5 " + colObj.name);
             damagableObj.MakeInvincible(respawnInvincibilityTimer);
-            Debug.Log("Handle Exit 6 " + colObj.name);
+            //Debug.Log("Handle Exit 6 " + colObj.name);
             damagableObj.Die();
-            Debug.Log("Handle Exit 7 " + colObj.name);
+            //Debug.Log("Handle Exit 7 " + colObj.name);
             damagableObj.currentDamage = 0;
-            Debug.Log("Handle Exit 8 " + colObj.name);
+            //Debug.Log("Handle Exit 8 " + colObj.name);
 
             int playerNumber = colObj.name == "Player-Couch-P1" ? 1 : 2;
             GameManager.Instance.PlayerEliminated(playerNumber);
 
-            colObj.transform.position = respawnPoint;
-            Debug.Log("Reset " + colObj.name + " to spawn point");
+            // Find the respawn position of the object
+            foreach(RespawnPosition rp in respawnPoints)
+            {
+                if(rp.gm.Equals(colObj))
+                {
+                    colObj.transform.position = rp.pos;
+                    break;
+                }
+            }
+            //Debug.Log("Reset " + colObj.name + " to spawn point");
         }
         else // Destroy everything that exits thats not a player
         {
@@ -69,5 +95,19 @@ public class handleExitPlay : MonoBehaviour
 
         debounceList.Remove(obj);
         Debug.Log(obj + " removed from the debounce list");
+    }
+
+    private IEnumerator GatherRespawnPoints()
+    {
+        yield return new WaitForEndOfFrame();
+
+        // Set the player spawn positions
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+        foreach (GameObject gm in players)
+        {
+            respawnPoints.Add(new RespawnPosition(gm, gm.transform.position));
+        }
+
+        yield return null;
     }
 }
